7d84ea8d08ca1bc4492270b9bde2ddbffc645b67
trilok <ram@hk.com>
2018-09-13 11:52:21 +0530

    Adhi sesha Naga Kanni Om

---
+++ __manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -GPK*-
+
+{
+    "name": "Shesha",
+    "version": "1.0",
+    "author": "La Mars",
+    "website": "http://",
+    "category": "Naga Kanni",
+    "sequence": 1,
+    "summary": "Hospital Management System",
+    "description": """
+    
+    Hospital Management System
+    
+    Patient Management
+    Employee Management
+    Purchase Management
+    Pharmacy Management
+    Assert Management
+    Accounts Management
+    
+    """,
+    "depends": ["base", "mail"],
+    "data": [
+        
+    ],
+    "demo": [
+
+    ],
+    "qweb": [
+
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": True,
+}
